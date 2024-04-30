62db1c8a1c64d7e66b11507dc82a6a96d84f3683
Ermias80 <ermiasmaz8069@gmail.com>
2024-04-30 09:27:34 -0700

    all

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -9,30 +9,33 @@
  */
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *curr, *node1, *node2;
+	listint_t *node = NULL, *tmp = NULL;
 
-	if (!list || !(*list))
+	if (list == NULL || *list == NULL || (*list)->next == NULL)
 		return;
-	curr = (*list)->next;
-	while (curr)
+
+	node = *list;
+	node = node->next;
+	while (node)
 	{
-		node1 = curr;
-		curr = curr->next;
-		while (node1->prev && node1->n < node1->prev->n)
+		while (node->prev && node->n < (node->prev)->n)
 		{
-			node2 = node1->prev;
-			if (node1->next)
-				node1->next->prev = node1->prev;
-			node1->prev->next = node1->next;
-			node1->prev = node2->prev;
-			node1->next = node2;
-			if (node2->prev)
-				node2->prev->next = node1;
-			else
-				*list = node1;
-			node2->prev = node1;
+			tmp = node;
+			if (node->next)
+				(node->next)->prev = tmp->prev;
+			(node->prev)->next = tmp->next;
+			node = node->prev;
+			tmp->prev = node->prev;
+			tmp->next = node;
+			if (node->prev)
+				(node->prev)->next = tmp;
+			node->prev = tmp;
+			if (tmp->prev == NULL)
+				*list = tmp;
 			print_list(*list);
-			node1 = node2;
+			node = node->prev;
 		}
+		node = node->next;
 	}
+
 }
