dac0f51ee2677f4092fd99d6154c61cb9ef93ea5
Ermias80 <ermiasmaz8069@gmail.com>
2024-04-30 12:14:25 -0700

    all

--- sort.h
+++ sort.h
@@ -25,7 +25,7 @@ void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
-void quicksort_lomuto(int *array, int left, int right, size_t size);
+void quicksort_lom(int *array, int left, int right, size_t size);
 int lomuto_partition(int *array, int left, int right, size_t size);
 void shell_sort(int *array, size_t size);
 void cocktail_sort_list(listint_t **list);
